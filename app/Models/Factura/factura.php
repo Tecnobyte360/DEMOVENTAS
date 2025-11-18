61d39f22d77964d8eecbccea7ccd194720d4db38
Tecnobyte360 <comercial@tecnobyte360.com>
2025-11-17 21:13:02 -0500

    se organiza el costo del asiento  contable

--- app/Models/Factura/factura.php
+++ app/Models/Factura/factura.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Factura;
 
+use App\Models\ConfiguracionEmpresas\Empresa;
 use App\Models\MediosPago\MedioPagos;
 use App\Models\Serie\Serie;
 use App\Models\SocioNegocio\SocioNegocio;
@@ -194,5 +195,10 @@ class Factura extends Model
 
             $this->recalcularTotales()->save();
         });
+    }
+     public function empresa(): BelongsTo
+    {
+        // si tu FK es empresa_id en la tabla facturas
+        return $this->belongsTo(Empresa::class, 'empresa_id');
     }
 }

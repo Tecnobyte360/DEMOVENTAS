bd956aafcc931a26ba4c75a8be84d6e626622e7a
Tecnobyte360 <comercial@tecnobyte360.com>
2025-12-08 20:33:20 -0500

    se realiza ajuste en el sidebar

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -156,7 +156,28 @@
                             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z"/>
                         </svg>
                     </button>
-
+  <div x-show="open" 
+                         x-collapse
+                         class="mt-1 ml-8 space-y-1 lg:hidden lg:sidebar-expanded:block 2xl:block">
+                        <a href="{{ route('Facturacion') }}" 
+                           class="group relative block px-3 py-2 rounded-lg text-sm font-medium 
+                                  transition-all duration-200 overflow-hidden
+                                  hover:bg-white/25 hover:pl-5 hover:shadow-sm
+                                  @if(Route::is('Facturacion')) bg-white/20 pl-5 @endif"
+                           :style="`color: ${fgMuted}`">
+                            <div class="absolute left-0 top-0 bottom-0 w-0.5 bg-white/0 group-hover:bg-white transition-all duration-300"></div>
+                            <span class="transition-all duration-200 group-hover:text-[#1a1a1a] group-hover:font-bold">Factura de venta</span>
+                        </a>
+                        <a href="{{ route('notascreditoclientes') }}" 
+                           class="group relative block px-3 py-2 rounded-lg text-sm font-medium 
+                                  transition-all duration-200 overflow-hidden
+                                  hover:bg-white/25 hover:pl-5 hover:shadow-sm
+                                  @if(Route::is('notascreditoclientes')) bg-white/20 pl-5 @endif"
+                           :style="`color: ${fgMuted}`">
+                            <div class="absolute left-0 top-0 bottom-0 w-0.5 bg-white/0 group-hover:bg-white transition-all duration-300"></div>
+                            <span class="transition-all duration-200 group-hover:text-[#1a1a1a] group-hover:font-bold">Nota crédito de ventas</span>
+                        </a>
+                    </div>
                 
                 </div>
 

cdfc8722253012e746d8a9f95f5bcd03b716f65c
Tecnobyte360 <comercial@tecnobyte360.com>
2026-01-02 19:34:21 -0500

    se realiza plan cuentas contables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ProductoCuentaTipoSeeder::class,
             UnidadMedidaSeeder::class,
             EntradasMercanciaTipoYSerieSeeder::class,
+            PlanCuentasActivosSeeder::class
         ]);
     }
 }

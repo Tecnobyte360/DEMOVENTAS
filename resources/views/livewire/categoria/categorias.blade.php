20ec27f03959c2a1d38df7be464ddfd90baaf5eb
Tecnobyte360 <comercial@tecnobyte360.com>
2026-04-28 20:01:35 -0500

    feat(categorias): habilita boton de eliminar con confirmacion en la tabla

--- resources/views/livewire/categoria/categorias.blade.php
+++ resources/views/livewire/categoria/categorias.blade.php
@@ -83,13 +83,14 @@
                             >
                                  <i class="fas fa-edit mr-1"></i> 
                             </button>
-                            {{-- <button
+                            <button
                                 wire:click="delete({{ $cat->id }})"
+                                wire:confirm="¿Eliminar la categoría '{{ $cat->nombre }}'? Esta acción no se puede deshacer."
                                 class="text-red-600 hover:text-red-800"
                                 title="Eliminar"
                             >
-                                 <i class="fas fa-trash-alt mr-1"></i>
-                            </button> --}}
+                                <i class="fas fa-trash-alt mr-1"></i>
+                            </button>
                         </td>
                     </tr>
                 @empty

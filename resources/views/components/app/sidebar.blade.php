2f294ef87ee8275c7004898330c36dc2df56b18f
Tecnobyte360 <comercial@tecnobyte360.com>
2026-04-28 19:21:18 -0500

    feat(saborboreal): enlace 'POS Venta rapida' en sidebar bajo Ventas

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -205,6 +205,17 @@
                                     </div>
                                     <span class="transition-all duration-200 group-hover:font-bold">Factura de venta</span>
                                 </a>
+
+                                <a href="{{ route('pos.factura') }}"
+                                    class="group relative block px-3 py-2 rounded-lg text-sm font-medium transition-all duration-200 overflow-hidden
+                                      hover:bg-white/25 hover:pl-5 hover:shadow-sm
+                                      @if (Route::is('pos.factura')) bg-white/20 pl-5 @endif"
+                                    :style="`color: ${fgMuted}`">
+                                    <div
+                                        class="absolute left-0 top-0 bottom-0 w-0.5 bg-white/0 group-hover:bg-white transition-all duration-300">
+                                    </div>
+                                    <span class="transition-all duration-200 group-hover:font-bold">POS Venta rápida</span>
+                                </a>
                             @endcan
 
                             @can('cotizaciones.ver')
